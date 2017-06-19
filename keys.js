4169c97e1de491eff946788fd0731590155dbf2c
Tami <tkapphahn@gmail.com>
2017-06-19 14:17:52 -0700

    LIRI set up per instructions

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: '3g8vpVv2JfaCT9NxBNxb0v6Kn',
+  consumer_secret: 'dAbpXgL27x2UBn1tcUdJ4RHpt4RdxQAVsq33Qirb15tZ5SrWUE',
+  access_token_key: '876902179373633536-hk40L7ORxhIYZVfhGnmqpG9cmEf52gM',
+  access_token_secret: 'HuTkbmLD2mBqD7fI7vd7vQkYQclSzDlbVTAEOFhSsDjTs',
+}
---
+++ liri.js
@@ -0,0 +1,26 @@
+//code to grab data from keys.js; store keys in a variable
+
+//liri.js should be able to take in one of following commands:
+//my-tweets - shows last 20 tweets and when they were created in Bash window
+
+//spotify-this-song '<song name here>'  shows artist(s), song's name, preview 
+//link of the song from Spotify, album that song is from.  If no song is provided,
+//then program defaults to "The Sign" by Ace of Base
+
+//movie-this '<movie name here>' output movie title, year, imdb rating, country
+//movie was produced, language of movie, plot, actors, rotten tomatoes url.  If
+// user doesn't type a move in, outputs data for 'Mr Nobody'  
+// use API key: 40e9cece
+
+//do-what-it-says.  using fs Node package, LIRI takes the text inside of random.txt
+// and uses it to call one of LIRIs commands.  It should run spotify-this-song 
+// for "I Want it That Way".  feel free to change the text in the random.txt doc 
+// to test out the features for other commands
+
+// ****************************
+//              BONUS
+//******************************
+// in addition to loggin data to your bash window, output data to a 
+// .txt file called log.txt  - make sure you append each command you run
+// to log.txt, do not overwrite your file each time you run a command
+
