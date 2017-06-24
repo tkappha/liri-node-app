fa7cbd6ddf6ae271768df8c05ed3d67238c6c7ff
Tami <tkapphahn@gmail.com>
2017-06-23 20:20:34 -0700

    mostly done

--- keys.js
+++ keys.js
@@ -1,8 +1,11 @@
+//regenerated 6/21/17 because I pushed to github before putting keys.js in the .gitignore...despite trying several times, will not 
+// let me regen the access tokens.
+
 console.log('this is loaded');
 
 exports.twitterKeys = {
-  consumer_key: '3g8vpVv2JfaCT9NxBNxb0v6Kn',
-  consumer_secret: 'dAbpXgL27x2UBn1tcUdJ4RHpt4RdxQAVsq33Qirb15tZ5SrWUE',
+  consumer_key: 'DnBMZO8bQXejWpH7RGY4rZ6Ok',
+  consumer_secret: 'ZnSDoWzHa2rp9VClUyf89vOwYDCMb9X7XJgJtXP6ZsxaXLwIxR',
   access_token_key: '876902179373633536-hk40L7ORxhIYZVfhGnmqpG9cmEf52gM',
   access_token_secret: 'HuTkbmLD2mBqD7fI7vd7vQkYQclSzDlbVTAEOFhSsDjTs',
 }
--- liri.js
+++ liri.js
@@ -1,11 +1,96 @@
 //code to grab data from keys.js; store keys in a variable
+var keys = require('./keys.js');
 
-//liri.js should be able to take in one of following commands:
-//my-tweets - shows last 20 tweets and when they were created in Bash window
 
+var Twitter = require('twitter');
+var spotify = require('spotify');
+var request = require('request');
+
+
+// ******** Twitter *********
+
+var getMyTweets = function() {
+	var client = new Twitter(keys.twitterKeys);
+ 
+	var params = {screen_name: 'izzi_samoyed'};
+	client.get('statuses/user_timeline', params, function(error, tweets, response) {
+	  if (!error) {
+	    // console.log(tweets);
+	    for(var i = 0; i < tweets.length; i++){
+	    	console.log(tweets[i].created_at);
+	    	console.log(tweets[i].text);
+	    	console.log("*******************************");
+	    }
+	  }
+	});
+
+}
+
+
+// ********   Spotify ********
+var getArtistNames = function(artist) {
+	return artist.name;
+}
+
+var getMySpotify = function(songName){
+
+		spotify.search({ type: 'track', query: songName }, function(err, data) {
+		    if ( err ) {
+		        console.log('Error occurred: ' + err);
+		        return;
+		    }
+		 
 //spotify-this-song '<song name here>'  shows artist(s), song's name, preview 
 //link of the song from Spotify, album that song is from.  If no song is provided,
 //then program defaults to "The Sign" by Ace of Base
+		   var songs = data.tracks.items;
+		   for(var i = 0; i < songs.length; i++) {
+		   	console.log(i);
+		   	console.log('artist(s): ' + songs[i].artists.map(getArtistNames));
+		   	console.log('song name: ' + songs[i].name);
+		   	console.log('preview song: ' + songs[i]. preview_url);
+		   	console.log('album: ' + songs[i].album.name);
+		   	console.log('*************************************************');
+		   }
+		});
+	}
+
+
+var getMyMovie = function(movieName){
+
+	request('http://www.omdbapi.com/?t='+ movieName +'&y=&plot=short&r=json', function (error, response, body) {
+	  if (!error && response.statusCode == 200){
+	  	console.log(body)
+	  } 
+	})
+}
+
+var pick = function(caseData, functionData){
+	switch(caseData) {
+		case 'my-tweets' :
+			getMyTweets();
+			break;
+
+		case 'spotify-this-song':
+			getMySpotify(functionData);
+			break;
+		case 'movie-this'
+			 getMyMovie(functionData);
+			 break;
+
+		default:
+			console.log("Not a recognized command");
+	}
+}
+//liri.js should be able to take in one of following commands:
+//my-tweets - shows last 20 tweets and when they were created in Bash window
+
+var runThis = function(argOne, argTwo){
+	pick(argOne, argTwo);
+};
+
+runThis(process.argv[2], process.argv[3]);
+
 
 //movie-this '<movie name here>' output movie title, year, imdb rating, country
 //movie was produced, language of movie, plot, actors, rotten tomatoes url.  If
